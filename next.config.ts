89a9214f9a32ad7c3375e84ad7ab0a5e4b922c26
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-09-28 19:02:49 +0200

    Update authentication URL in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
         fetches: {
             fullUrl: true
         }
+    },
+    env: {
+        AUTH_URL: process.env.VERCEL_URL
+            ? `https://${process.env.VERCEL_URL}`
+            : 'http://localhost:3000'
     }
 }
 
