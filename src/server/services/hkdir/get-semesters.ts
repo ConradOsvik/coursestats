bb0a457a0a1d1e6bdf2052610b867640913ccab3
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-02-20 02:07:00 +0100

    remove old HKDIR service implementation

--- src/server/services/hkdir/get-semesters.ts
+++
@@ -1,5 +0,0 @@
-import { fetchData } from "~/server/services/hkdir/client";
-import { formatSemesterData } from "~/server/services/hkdir/helpers";
-
-export const getSemesters = async (id: string) =>
-  fetchData(id).then(formatSemesterData);
