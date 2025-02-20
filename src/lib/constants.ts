0491212768b25796e8cfd97f7937797e84218c58
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2025-02-20 02:06:10 +0100

    add constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,9 @@
+export const SEMESTERS = ["spring", "fall"] as const;
+
+export const INSTITUTIONS = [
+  {
+    name: "Norges teknisk-naturvitenskapelige universitet",
+    initial: "NTNU",
+    id: 1150,
+  },
+] as const;
